9bfa1d5d2eebd198f435107f0115102a912f99c3
MOUSTAPHA SOW <105611478+Moustaphasow01@users.noreply.github.com>
2023-01-24 23:27:09 +0100

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
 """
-Import modules
+Script to gather data from an API
 """
 import requests
 from sys import argv
 
-
-def api():
-    """
-    Return API data
-    """
-    users = requests.get("http://jsonplaceholder.typicode.com/users")
-    for u in users.json():
-        if u.get('id') == int(argv[1]):
-            EMPLOYEE_NAME = (u.get('name'))
-            break
-    TOTAL_NUM_OF_TASKS = 0
-    NUMBER_OF_DONE_TASKS = 0
-    TASK_TITLE = []
-    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
-    for t in todos.json():
-        if t.get('userId') == int(argv[1]):
-            TOTAL_NUM_OF_TASKS += 1
-            if t.get('completed') is True:
-                NUMBER_OF_DONE_TASKS += 1
-                TASK_TITLE.append(t.get('title'))
-    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
-                                                          NUMBER_OF_DONE_TASKS,
-                                                          TOTAL_NUM_OF_TASKS))
-    for task in TASK_TITLE:
-        print("\t {}".format(task))
-
-
+response_API = 'https://jsonplaceholder.typicode.com/'
 if __name__ == "__main__":
-    api()
+    users = requests.get("{}users/{}".format(response_API, argv[1])).json()
+    todos = requests.get("{}todos?userId={}".format(
+        response_API, argv[1])).json()
+
+    task_completed = [task['title'] for task in todos if task['completed']]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(users["name"], len(task_completed), len(todos)))
+    print("\n".join("\t {}".format(done) for done in task_completed))
